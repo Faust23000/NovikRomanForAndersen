4273cbbef1207be714ec40035d435435b74c4560
Roman Novik <novic10@mail.ru>
2020-12-22 03:08:07 +0300

    ок

--- NovikRomanForAndersen/Program.cs
+++ NovikRomanForAndersen/Program.cs
@@ -6,52 +6,27 @@ namespace NovikRomanForAndersen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Task1");              // Задача №1
-            Console.WriteLine("");
             Console.WriteLine("Введите число:");
-            int num = Convert.ToInt32(Console.ReadLine());
-            if (num > 7)
-            {
-                Console.WriteLine("Привет");
-            }
+            int num; 
+            string input = Console.ReadLine();
 
-            Console.WriteLine("");                   // Задача №2
-            Console.WriteLine("Task2");
-            Console.WriteLine("");
-            Console.WriteLine("Введите имя:");
-            string name = Console.ReadLine();
-            if (name == "Вячеслав")
+            if (Int32.TryParse(input, out num))
             {
-                Console.WriteLine($"Привет, {name}");
+                if (num > 7)
+                {
+                    Console.WriteLine("Привет");
+                }
+                else
+                {
+                    Console.WriteLine("Введено неверное число, начните заново");
+                }
             }
             else
             {
-                Console.WriteLine("Нет такого имени");
+                Console.WriteLine("Некорректное значение");
             }
-
-            Console.WriteLine("");                   // Задача №3
-            Console.WriteLine("Task3");
-            Console.WriteLine("");
-            int[] numbers = new int[] { 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 };
-            Console.Write("Массив:");
-            foreach (int i in numbers)
-            {
-                Console.Write(i);
-                Console.Write(",");
-            }
-            Console.WriteLine("");
-            Console.WriteLine("Нажмите Enter");
             Console.ReadKey();
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                if (numbers[i] % 3 == 0)
-                {
-                    Console.WriteLine(numbers[i]);
-                }
-            }
 
-            Console.Beep();
-            Console.ReadKey();
         }
     }
 }
---
+++ Task_3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива:");
+            int n;
+            string input = Console.ReadLine();
+            if (Int32.TryParse(input, out n))
+            {
+                int[] numbers = new int[n];
+                for (int i = 0; i < n; i++)                             // Заполяем массив
+                {
+                    try
+                    {
+                        Console.WriteLine("Введите {0} элемент массива:", i);
+                        numbers[i] = Convert.ToInt32(Console.ReadLine());
+                        
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Введено неверное значение");
+                    }
+                }
+                foreach (int i in numbers)                               // Просматриваем заполненный массив
+                {
+                    Console.Write(i);
+                    Console.Write(" ");
+                }
+                Console.WriteLine("");
+                Console.WriteLine("Нажмите Enter");
+                Console.WriteLine("");
+                Console.ReadKey();
+                for (int i = 0; i < numbers.Length; i++)                  // Ищем значения кратные 3
+                {
+                    if (numbers[i] % 3 == 0 && numbers[i] != 0)
+                    {
+                        Console.WriteLine(numbers[i]);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введено неверное значение");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
